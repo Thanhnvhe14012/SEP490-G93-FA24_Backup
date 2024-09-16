ff5f0c201c80443250760d53177be92f998f7478
thanhnvhe140127 <thanhnvhe140127@fpt.edu.vn>
2024-09-16 17:24:06 +0700

    fist commit

---
+++ src/test/java/com/fpt/quickhire/QuickHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.quickhire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
